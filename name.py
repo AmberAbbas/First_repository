fba66f4411a930c21e0ad1b65c1b6688748f55fc
Amber Abbas <amberabbas272@gmail.com>
2022-07-12 23:56:16 +0530

    Added a promt to ask the user for their city

--- name.py
+++ name.py
@@ -1,3 +1,15 @@
+from numpy import place
+from pandas import array
+
+
 name= input("What is your name?")
 print("Welcome", name)
 print("It is fantastic to have you here!")
+
+place= input("Where do you live?")
+if place in ["Delhi", "Mumbai", "Hyderabad", "Banglore"]:
+    print("You belong to the four big cities of India.")
+    
+
+
+
